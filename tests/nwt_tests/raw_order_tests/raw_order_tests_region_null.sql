841d26d757ceaf02473351181120f31f8bf27ab1
cheryl :) <S10241900@connect.np.edu.sg>
2024-01-11 08:43:39 +0000

    updated raw order tests for region_null and date

--- tests/nwt_tests/raw_order_tests/raw_order_tests_region_null.sql
+++ tests/nwt_tests/raw_order_tests/raw_order_tests_region_null.sql
@@ -1,1 +1,9 @@
---
+with
+orders as (
+    select * from {{ ref('raw_orders') }}
+)
+
+select * from orders
+where shipRegion LIKE '%NULL%'
+
+-- shipregion has null but do not need to clean because creating new column for it
--- tests/nwt_tests/raw_order_tests/raw_order_tests_requiredDate_higher_than_orderDate.sql
+++ tests/nwt_tests/raw_order_tests/raw_order_tests_requiredDate_higher_than_orderDate.sql
@@ -1,1 +1,9 @@
---
+with
+orders as (
+    select * from {{ ref('raw_orders') }}
+)
+
+select * from orders
+where requiredDate < orderDate
+
+--there is no wrong date ordering, so nothing to clean here
