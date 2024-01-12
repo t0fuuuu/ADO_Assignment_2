e126852023780e5d1becf53905f81c6468f430d9
Ryan Ma <s10242111@connect.np.edu.sg>
2024-01-12 08:07:31 +0000

    ok bro

--- models/cleaning/raw_supplier_clean.sql
+++ models/cleaning/raw_supplier_clean.sql
@@ -1,15 +1,15 @@
--- Replace NULL values in specified columns with default values
-UPDATE NWT_RAW.NWT_DISTRIBUTOR.RAW_SUPPLIER
-SET REGION = 'Unknown'
-WHERE REGION = 'NULL';
+-- -- Replace NULL values in specified columns with default values
+-- UPDATE NWT_RAW.NWT_DISTRIBUTOR.RAW_SUPPLIER
+-- SET REGION = 'Unknown'
+-- WHERE REGION = 'NULL';
 
--- Standardize the city column
-UPDATE NWT_RAW.NWT_DISTRIBUTOR.RAW_SUPPLIER
-SET CITY = INITCAP(CITY);
+-- -- Standardize the city column
+-- UPDATE NWT_RAW.NWT_DISTRIBUTOR.RAW_SUPPLIER
+-- SET CITY = INITCAP(CITY);
 
--- Standardize capitalisation (title case)
-UPDATE NWT_RAW.NWT_DISTRIBUTOR.RAW_SUPPLIER
-SET COMPANYNAME = INITCAP(COMPANYNAME);
+-- -- Standardize capitalisation (title case)
+-- UPDATE NWT_RAW.NWT_DISTRIBUTOR.RAW_SUPPLIER
+-- SET COMPANYNAME = INITCAP(COMPANYNAME);
 
 
 ---- Standardize the country column (replace with consistent spellings)
