797cdf6194d5f1c58836d30f83f26ad772991514
Cheryl Neo <S10241900@connect.np.edu.sg>
2024-01-22 08:10:16 +0000

    just added comments in tests

--- tests/nwt_tests/raw_customer_tests/raw_customer_tests_region_null.sql
+++ tests/nwt_tests/raw_customer_tests/raw_customer_tests_region_null.sql
@@ -5,3 +5,5 @@ customer as (
 
 select * from customer
 where region LIKE '%NULL%'
+--found out region have null and some regions are inaccurate (wrong region), hence we gnna make a new column for dervied_region
+--fax also have null but we are js not gonna bother cleaning it
